1684549d7a5c9596ba57052920e1f8717ec0003c
abrilrodriguez <abril@octopus.ru>
2018-04-24 17:26:46 -0500

    Screen 1

---
+++ Nebulosa/Nebulosa/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace Nebulosa
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+
+			InitializeComponent();
+		}
+	}
+}
