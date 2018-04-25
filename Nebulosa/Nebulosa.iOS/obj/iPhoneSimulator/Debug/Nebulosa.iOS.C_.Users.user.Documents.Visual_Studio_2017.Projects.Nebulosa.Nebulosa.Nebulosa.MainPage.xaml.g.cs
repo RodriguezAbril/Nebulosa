c145712f205fd65c55fc231913e8e49d4e39cba6
abrilrodriguez <abril@octopus.ru>
2018-04-25 18:29:44 -0500

    se agrego el grafico

--- Nebulosa/Nebulosa.iOS/obj/iPhoneSimulator/Debug/Nebulosa.iOS.C_.Users.user.Documents.Visual_Studio_2017.Projects.Nebulosa.Nebulosa.Nebulosa.MainPage.xaml.g.cs
+++ Nebulosa/Nebulosa.iOS/obj/iPhoneSimulator/Debug/Nebulosa.iOS.C_.Users.user.Documents.Visual_Studio_2017.Projects.Nebulosa.Nebulosa.Nebulosa.MainPage.xaml.g.cs
@@ -16,8 +16,12 @@ namespace Nebulosa {
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private global::Xamarin.Forms.Image CircleImageAvatar;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
+            CircleImageAvatar = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Image>(this, "CircleImageAvatar");
         }
     }
 }
